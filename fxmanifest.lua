c35f68ef020b3fd5278455fb1cd1e1d1028280b5
Bambi <ambermoulayess@gmail.com>
2022-10-03 12:26:46 +0300

    mod(galaxy-clothingpack): Changes (F)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 files {
-  "mp_f_freemode_01.meta",
+  "mp_f_freemode_01_test.meta",
   
 }
 
-data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_test.meta"
